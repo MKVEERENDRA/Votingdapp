cdd40554fc5143e5201d219318397e8c1c49da1a
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2025-01-01 11:18:35 +0530

    Update Voter.js

--- context/Voter.js
+++ context/Voter.js
@@ -5,7 +5,7 @@ import Web3Modal from "web3modal";
 import { useRouter } from "next/router";
 import {votingAddress,votingABI} from "../context/constants";
 const fetchContract = (signerOrProvider) => new ethers.Contract(votingAddress, votingABI, signerOrProvider);
-const uei="https://polygon-amoy.g.alchemy.com/v2/BTeFNsqvZQ3n6WacRdUT1P4U-KSXkaOA";
+const uei="https://polygon-amoy.g.alchemy.com/v2/jyDIa4u5Su99z0yPRYvc6uVm0sljrAwU";
 export const VotingContext =React.createContext();
 export const VotingProvider = ({children})=>{
     const votingTitle ="My dapp  ";
