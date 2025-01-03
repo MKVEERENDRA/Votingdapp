e2509c27a5c9611f38538ab3326cb1b296a3a136
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2025-01-03 19:49:58 +0530

    Update constants.js

--- context/constants.js
+++ context/constants.js
@@ -1,7 +1,7 @@
 ///0x5FbDB2315678afecb367f032d93F642f64180aa3///
 import nftMarketPlace from "./NFTMarketPlace.json";
 
-export const votingAddress ="0x61083BE612b090111645d3a7c3854bBA5D06879b";
+export const votingAddress ="0x43B572154C42b7CF6fEe6b9c488912F18D9B345B";
 
 
 export const votingABI =nftMarketPlace.abi;
