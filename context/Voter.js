9e60d396c113a80b0d8f46fda4546fb34a834723
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2025-01-05 15:25:23 +0530

    Update Voter.js
    changed from google to alchamy

--- context/Voter.js
+++ context/Voter.js
@@ -5,7 +5,7 @@ import Web3Modal from "web3modal";
 import { useRouter } from "next/router";
 import {votingAddress,votingABI} from "../context/constants";
 const fetchContract = (signerOrProvider) => new ethers.Contract(votingAddress, votingABI, signerOrProvider);
-const uei="https://blockchain.googleapis.com/v1/projects/coherent-flame-426016-d3/locations/asia-east1/endpoints/ethereum-holesky/rpc?key=AIzaSyDUQhGwrAke5ui47BeViW-9RzhPC6mely0";
+const uei="https://eth-sepolia.g.alchemy.com/v2/jyDIa4u5Su99z0yPRYvc6uVm0sljrAwU";
 const alchemyProvider = new ethers.providers.JsonRpcProvider(uei);
 export const VotingContext =React.createContext();
 export const VotingProvider = ({children})=>{
